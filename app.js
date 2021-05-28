6f5202c010a9f2cd90ddcaf396bcf25badb31c47
Michael Carr <mcarr2468@gmail.com>
2021-05-28 17:32:28 -0500

    V1.1

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 import apiKey from "./apiKey.js";
-console.log("test")
+
+/*the api i'll be using
+    https://spoonacular.com/food-api/docs
+ */
+
+/*
+`https://api.spoonacular.com/recipes/716429/information?apiKey=${apiKey}&includeNutrition=false
+https://api.spoonacular.com/recipes/complexSearch
+*/
+
+// get the food choses eg: different types of cake
+    // fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query="cake"`)
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data)
+    //     })
+
+// carrot cake id 657968
+// get the recipe of the food you want
+    //https://api.spoonacular.com/recipes/716429/information?includeNutrition=false
+    // fetch(`https://api.spoonacular.com/recipes/657968/information?apiKey=${apiKey}`)
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data)
+    //     })
